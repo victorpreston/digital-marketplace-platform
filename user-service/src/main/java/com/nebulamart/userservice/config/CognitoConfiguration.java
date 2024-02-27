c67ef5ae2d978245bad88d9f03ebdb03c2f10876
Victor Preston <prestonvictor25@gmail.com>
2024-02-27 09:07:10 +0530

    sign in, security filter chain

--- user-service/src/main/java/com/nebulamart/userservice/config/CognitoConfiguration.java
+++ user-service/src/main/java/com/nebulamart/userservice/config/CognitoConfiguration.java
@@ -6,18 +6,23 @@ import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
 import software.amazon.awssdk.auth.credentials.AwsCredentialsProvider;
 import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
 import software.amazon.awssdk.services.cognitoidentityprovider.CognitoIdentityProviderClient;
+import org.springframework.beans.factory.annotation.Value;
 
 import software.amazon.awssdk.regions.Region;
 
 @Configuration
 public class CognitoConfiguration {
 
+    @Value("${aws-role-user.access-key}")
+    private String accessKey;
 
+    @Value("${aws-role-user.secret-key}")
+    private String secretKey;
 
     @Bean
     public CognitoIdentityProviderClient cognitoIdentityProviderClient() {
         AwsCredentialsProvider awsCredentialsProvider = StaticCredentialsProvider.create(
-                AwsBasicCredentials.create("${AWS_ACCESS_KEY}", "${AWS_SECRET_KEY}"));
+                AwsBasicCredentials.create(accessKey, secretKey));
 
         return CognitoIdentityProviderClient.builder()
                 .region(Region.US_EAST_1)
